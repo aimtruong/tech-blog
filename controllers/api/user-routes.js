5e85453cde16e03a16b95d905a36cd3a5b485620
Aimee Truong <aimtruong@ucdavis.edu>
2022-03-28 17:33:03 -0700

    user CRUD works, post and comments in progress

---
+++ controllers/api/user-routes.js
@@ -0,0 +1,156 @@
+
+const router = require("express").Router();
+const { User, Post, Comment } = require("../../models");
+
+
+// GET all users
+router.get("/", (req, res) => {
+    // access User model and run .findAll() method
+    User.findAll({
+        attributes: { exclude: ["password"] }
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// GET a user
+router.get("/:id", (req, res) => {
+    User.findOne({
+        attributes: { exclude: ["password"] },
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+                model: Comment,
+                attributes: ["id", "comment_text", "created_at"],
+                include: {
+                    model: Post,
+                    attributes: ["title"]
+                }
+            }
+        ]
+    })
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({ message: "No user found with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// POST all users
+router.post("/", (req, res) => {
+    User.create({
+        username: req.body.username,
+        password: req.body.password
+    })
+        .then(dbUserData => {
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json(dbUserData);
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// POST user's login; /api/users/login
+router.post("/login", (req, res) => {
+    User.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(400).json({ message: "Username not found!" });
+                return;
+            }
+
+            const validPassword = dbUserData.checkPassword(req.body.password);
+            if(!validPassword){
+                res.status(400).json({ message: "Incorrect password!" });
+                return;
+            }
+
+            req.session.save(() => {
+                req.session.user_id = dbUserData.id;
+                req.session.username = dbUserData.username;
+                req.session.loggedIn = true;
+
+                res.json({ user: dbUserData, message: "You are now logged in!" });
+            });
+        });
+});
+
+// POST a logout
+router.post("/logout", (req, res) => {
+    if(req.session.loggedIn){
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    }
+    else{
+        res.status(404).end();
+    }
+});
+
+// PUT a user
+router.put("/:id", (req, res) => {
+    // if req.body has exact key/value pairs to match model, can just use req.body instead
+    User.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if(!dbUserData[0]){
+                res.status(400).json({ message: "No user found with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// DELETE a user
+router.delete("/:id", (req, res) => {
+    User.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if(!dbUserData){
+                res.status(404).json({ message: "No user found with this id" });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+
+module.exports = router;
+
