cf166b5faf31d9ff09fc39ad49c1c0ef3ed2ae7a
Aimee Truong <aimtruong@ucdavis.edu>
2022-03-29 15:09:17 -0700

    handlebars files work, just need to add post-content text

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -2,14 +2,14 @@
 async function loginFormHandler(event) {
     event.preventDefault();
   
-    const email = document.querySelector('#email-login').value.trim();
+    const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (email && password) {
+    if (username && password) {
         const response = await fetch('/api/users/login', {
             method: 'post',
             body: JSON.stringify({
-                email,
+                username,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
@@ -28,15 +28,13 @@ async function signupFormHandler(event) {
     event.preventDefault();
   
     const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
   
-    if (username && email && password) {
+    if (username && password) {
         const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
-                email,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
@@ -44,7 +42,7 @@ async function signupFormHandler(event) {
   
         // check the response status
         if (response.ok) {
-            console.log('success');
+            document.location.replace('/');
         }
         else {
             alert(response.statusText);
