2ddd04da7732b52bfce862ecf401f8dfb951e0cd
Aimee Truong <aimtruong@ucdavis.edu>
2022-03-31 00:00:29 -0700

    change port in connection

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306
+        port: 3301
     });
 }
 
