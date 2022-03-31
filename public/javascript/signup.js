3303452db733ea1cc864d2dfd182cda96ae9cd79
Aimee Truong <aimtruong@ucdavis.edu>
2022-03-30 19:40:10 -0700

    login and signup handlebars/js/style added and fixed

--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -1,20 +1,20 @@
-
-async function loginFormHandler(event) {
+async function signupFormHandler(event) {
     event.preventDefault();
   
-    const username = document.querySelector('#username-login').value.trim();
-    const password = document.querySelector('#password-login').value.trim();
+    const username = document.querySelector('#username-signup').value.trim();
+    const password = document.querySelector('#password-signup').value.trim();
   
     if (username && password) {
-        const response = await fetch('/api/users/login', {
+        const response = await fetch('/api/users', {
             method: 'post',
             body: JSON.stringify({
                 username,
                 password
             }),
             headers: { 'Content-Type': 'application/json' }
-      });
+        });
   
+        // check the response status
         if (response.ok) {
             document.location.replace('/dashboard');
         }
@@ -25,5 +25,4 @@ async function loginFormHandler(event) {
 };
 
 
-document.querySelector(".login-form").addEventListener("submit", loginFormHandler);
-
+document.querySelector(".signup-form").addEventListener("submit", signupFormHandler);
