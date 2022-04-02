56d97ee79143fe131d8636e1fda5f25ef4e6e551
Aimee Truong <aimtruong@ucdavis.edu>
2022-04-02 12:24:49 -0700

    removed process.env from connection

--- config/connection.js
+++ config/connection.js
@@ -13,7 +13,7 @@ else {
     sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
         host: 'localhost',
         dialect: 'mysql',
-        port: process.env.PORT || 3306
+        port: 3306
     });
 }
 
